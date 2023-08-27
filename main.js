9de845b33a7466ae403091bf79683586ccbe5dd2
alihakimov919@gmail.com <alihakimov919@gmail.com>
2023-08-27 06:04:46 +0600

    first commit

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-function nav(delay) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const nav = document.createElement('div');
-        nav.className = 'nav';
-        document.getElementById('container').appendChild(nav);
-        resolve(); 
-      }, delay);
-    });
-  }
-  
-  async function black() {
-    for (let i = 0; i < 4; i++) {
-      await nav(1000); 
-    }
-  }
-  
-  black();
+const promise = new Promise ((resolve, reject) => {
+  const errorIndicator = false  ;
+
+  setTimeout(() => {
+   if (!errorIndicator) {
+    const jsonData = {
+      username: 'Ali',
+      userage: 16,
+    };
+
+    const strJsonData = JSON.stringify(jsonData);
+    console.log(strJsonData);
+
+    resolve(strJsonData);
+   } else {
+    reject('Oops something went wrong');
+   }
+  }, 2000);
+});
+
+promise
+.then((data) => {
+  console.log(data);
+  setTimeout(() => {
+   const parseData = JSON.parse(data);
+    console.log(parseData);
+  }, 2000)
+})
+ .catch(() => {
+  console.log('Preparing data...');
+ })
